2d46fc1940d90700a18faba9291ba2e913663d7c
Louis Pilfold <louis@lpil.uk>
2015-01-08 16:21:03 +0000

    OpenCV is impossible to compile.

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to decoder
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/decoder/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns decoder.core-test
-  (:require [clojure.test :refer :all]
-            [decoder.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
