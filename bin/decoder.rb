405fcca8ece2d07c26a796fcbc0daaaea3fdf454
Louis Pilfold <louis@lpil.uk>
2015-01-09 17:56:08 +0000

    Replace missing var

--- bin/decoder.rb
+++ bin/decoder.rb
@@ -5,10 +5,11 @@ include OpenCV
 
 abort "Usage: #{__FILE__} path/to/video" if ARGV.empty?
 
+video_file = ARGV.first
 begin
-  video_stream = CvCapture.open ARGV.first
+  stream = CvCapture.open video_file
 rescue
   abort "Can't find or read '#{video_file}'!"
 end
 
-puts "There are #{video_stream.frame_count} frames in '#{video_file}'"
+puts "There are #{stream.frame_count} frames in '#{video_file}'"
